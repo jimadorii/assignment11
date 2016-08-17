4f30d944159e4f79ff80d43126934f8060b1b909
Inoel Rivera <jimadoriicrafts@gmail.com>
2016-08-16 23:34:35 -0700

    Assignment 11 files and folders committed to repo

---
+++ assignment11-work/part3/json.js
@@ -0,0 +1,27 @@
+{"pets":[
+	{
+		"name":"Tyler",
+		"species":"Canine",
+		"breed":"Fox-red Labrador."
+	},
+	{
+		"name":"Jonas",
+		"species":"Feline Species",
+		"breed":"Shorthair Gray Tabby."
+	},
+	{
+		"name":"Olivia",
+		"species":"Feline Species",
+		"breed":"Longhair mix breed."
+	},
+	{
+		"name":"Oliver",
+		"species":"Feline Species",
+		"breed":"Shorthair mix breed."
+	},
+	{
+		"name":"Hamsterella",
+		"species":"Hamster",
+		"breed":"Syrian shorthair mulitcolor."
+	}
+]}
